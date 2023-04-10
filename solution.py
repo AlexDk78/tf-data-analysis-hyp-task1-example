c54758715f292f1fab906794dd8bbf8aa0c4a38d
Alexei Borovoi <81176384+AlexDk78@users.noreply.github.com>
2023-04-10 19:20:12 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
-
+from scipy import stats
+import math
 
 chat_id = 897113152 # Ваш chat ID, не меняйте название переменной
 
@@ -8,14 +9,22 @@ def solution(x_success: int,
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    import scipy.stats as stats
-    alpha = 0.08
+
+    alpha=0.08
+    
     p1 = x_success / x_cnt
     p2 = y_success / y_cnt
-    se = ((p1 * (1 - p1)) / x_cnt + (p2 * (1 - p2)) / y_cnt) ** 0.5
-    z = (p1 - p2) / se
-    z_crit = abs(stats.norm.ppf(alpha / 2))
-    if abs(z) > z_crit:
-        return True
-    else:
-        return False
+    p_combined = (x_success + y_success) / (x_cnt + y_cnt)
+    difference = p1 - p2
+ 
+    z_value = difference / math.sqrt(
+        p_combined * (1 - p_combined) * (1 / x_success + 1 / y_success)
+    ) 
+    distr = stats.norm(0, 1)  
+    
+    p_value = (1 - distr.cdf(abs(z_value))) * 2 
+    
+    if (p_value < alpha):
+      return True
+    else: 
+      return False
